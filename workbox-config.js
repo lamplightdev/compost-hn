82a6b0bf9bf76e59b2475b38b28b08912494c91f
Chris Haynes <chris.haynes@gmail.com>
2018-02-07 22:30:59 +0000

    Remove unwanted sw precache files

--- workbox-config.js
+++ workbox-config.js
@@ -2,9 +2,8 @@ module.exports = {
   "globDirectory": "public/",
   "globPatterns": [
     "build/app.js",
-    "build/libs/webcomponentsjs/*.js",
-    "images/*",
-    "manifest.json",
+    "build/libs/webcomponentsjs/webcomponents-loader.js",
+    "images/*.svg",
     "index.html",
   ],
   "swDest": "public/sw.js",
